e7e1f3d2f9b07b93d8fadc1fb03c70fff837f2c4
Francesco Di Chiara <fdc@yelp.com>
2017-05-10 06:31:21 -0700

    Fix comment

--- signalform_tools/signalform.py
+++ signalform_tools/signalform.py
@@ -35,7 +35,7 @@ def parse_args():
         'preflight',
         help='preflight help',
         description='Test your detector.')
-    parser_preflight.add_argument('file', help='You do not have to specify this, use state=plan or state=apply', type=str)
+    parser_preflight.add_argument('file', help='Path to tfstate file', type=str)
     parser_preflight.add_argument('--label', help='Specific detect label to test, checks all in the current folder by default', type=str)
     parser_preflight.add_argument('--start', help='Start time to check from. Can be either SignalFx relative time format (e.g. "-60m", "-3d", "-1w"), a date or a UNIX epoch timestamp in seconds or milliseconds', type=str)
     parser_preflight.add_argument('--stop', help='End time to check until. Can be either SignalFx relative time format (e.g. "Now", "-60m", "-3d"), a date or a UNIX epoch timestamp in seconds or milliseconds', type=str)
